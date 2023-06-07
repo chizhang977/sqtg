54d272cd60dfbf65745c5d9c66c557293b331117
justin-cheng <justin-cheng@163.com>
2023-06-07 11:32:24 +0800

    初始化项目，添加基本配置类

---
+++ common/service-util/src/main/java/com/justin/sqtg/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.justin.sqtg.common.exception;
+
+import com.justin.sqtg.common.result.Result;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public Result error(Exception e){
+        e.printStackTrace();
+        return Result.fail(null);
+    }
+
+    /**
+     * 自定义异常处理方法
+     * @param e
+     * @return
+     */
+    /**
+     * 自定义异常处理方法
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(SsyxException.class)
+    @ResponseBody
+    public Result error(SsyxException e){
+        return Result.build(null,e.getCode(), e.getMessage());
+    }
+}
